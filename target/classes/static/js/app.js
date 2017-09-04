78b227a2f338b14697d955f186a57b3bd1a384ab
mujeebula <mujeeb68022@gmail.com>
2017-09-04 18:38:11 +0530

    Added functionality to add a new contact and start conversation

--- target/classes/static/js/app.js
+++ target/classes/static/js/app.js
@@ -8,6 +8,8 @@ var lastName;
 var contacts;
 var messages;
 var privateMessage;
+var searchResults;
+var contactSearchResults;
 (function() {
 	socket = new SockJS('http://localhost:8090/gs-guide-websocket');
 	stompClient = Stomp.over(socket);
@@ -24,7 +26,8 @@ var privateMessage;
 function subscribe() {
 	console.log("Subscribing topics");
 	stompClient.subscribe('/topic/greeting-' + sessionId, handleGreetings);
-	stompClient.subscribe('/topic/privateMessage-' + sessionId, handlePrivateMessage);
+	stompClient.subscribe('/topic/privateMessage-' + sessionId,
+			handlePrivateMessage);
 	stompClient.subscribe('/topic/contacts-' + sessionId, handleContacts);
 	stompClient.subscribe('/topic/userDetails-' + sessionId, handleUserDetails);
 	stompClient.subscribe('/topic/search-' + sessionId, handleSearch);
@@ -33,89 +36,116 @@ function subscribe() {
 	}));
 }
 
-function handlePrivateMessage(message){
+function handlePrivateMessage(message) {
 	privateMessage = JSON.parse(message.body);
 	console.log("From:/topic/privateMessage-->" + privateMessage);
 	insertPrivateMessage();
 }
 
-function handleUserDetails(userDetails){
+function handleUserDetails(userDetails) {
 	console.log("/topic/userDetails-->" + userDetails.body);
 	userDetails = JSON.parse(userDetails.body);
 	username = userDetails.username;
 	id = userDetails.id;
 	firstName = userDetails.firstName;
 	lastName = userDetails.lastName;
-	$("#myusername").text("#"+id+" "+username);
+	$("#myusername").text("#" + id + " " + username);
 }
 
-function handleGreetings(greetings){
+function handleGreetings(greetings) {
 	console.log("From:/topic/greeting-->" + greetings.body);
 	messages = JSON.parse(greetings.body);
 	console.log(messages);
 	insertMessages();
 }
 
-function insertMessages(){
+function insertMessages() {
 	$('#conversation').html("");
-	for(var i = 0; i < messages.length; i++){
+	for (var i = 0; i < messages.length; i++) {
 		var dateTime = new Date(messages[i].created);
 		dateTime = getFormattedDateTime(dateTime);
-		console.log(dateTime);
-		if(messages[i].senderId == id){
-			$('#conversation').append('<div class="row message-body"><div class="col-sm-12 message-main-sender"><div class="sender"><div class="message-text">'+
-					messages[i].content + '</div><span class="message-time pull-right">' + dateTime + '</span></div></div></div>');
-		}else{
-			$('#conversation').append('<div class="row message-body"><div class="col-sm-12 message-main-receiver"><div class="receiver"><div class="message-text">'+
-					messages[i].content + '</div><span class="message-time pull-right">' + dateTime + '</span></div></div></div>');
+		if (messages[i].senderId == id) {
+			$('#conversation')
+					.append(
+							'<div class="row message-body"><div class="col-sm-12 message-main-sender"><div class="sender"><div class="message-text">'
+									+ messages[i].content
+									+ '</div><span class="message-time pull-right">'
+									+ dateTime + '</span></div></div></div>');
+		} else {
+			$('#conversation')
+					.append(
+							'<div class="row message-body"><div class="col-sm-12 message-main-receiver"><div class="receiver"><div class="message-text">'
+									+ messages[i].content
+									+ '</div><span class="message-time pull-right">'
+									+ dateTime + '</span></div></div></div>');
 		}
 	}
 	$('#conversation').animate({
-	    scrollTop: $('#conversation').get(0).scrollHeight
+		scrollTop : $('#conversation').get(0).scrollHeight
 	}, 1500);
 }
 
-function insertPrivateMessage(){
-	console.log("Inserting private message:" + privateMessage);
-	var dateTime = new Date(privateMessage.created);
-	dateTime = getFormattedDateTime(dateTime);
-	console.log(dateTime);
-	if(privateMessage.senderId == id){
-		$('#conversation').append('<div class="row message-body"><div class="col-sm-12 message-main-sender"><div class="sender"><div class="message-text">'+
-				privateMessage.content + '</div><span class="message-time pull-right">' + dateTime + '</span></div></div></div>');
+function insertPrivateMessage() {
+	if (currentChatUserId == privateMessage.senderId || currentChatUserId == privateMessage.receiverId) {
+		console.log("Inserting private message:" + privateMessage);
+		var dateTime = new Date(privateMessage.created);
+		dateTime = getFormattedDateTime(dateTime);
+		console.log(dateTime);
+		if (privateMessage.senderId == id) {
+			$('#conversation')
+					.append(
+							'<div class="row message-body"><div class="col-sm-12 message-main-sender"><div class="sender"><div class="message-text">'
+									+ privateMessage.content
+									+ '</div><span class="message-time pull-right">'
+									+ dateTime + '</span></div></div></div>');
+		} else {
+			$('#conversation')
+					.append(
+							'<div class="row message-body"><div class="col-sm-12 message-main-receiver"><div class="receiver"><div class="message-text">'
+									+ privateMessage.content
+									+ '</div><span class="message-time pull-right">'
+									+ dateTime + '</span></div></div></div>');
+		}
+		$('#conversation').animate({
+			scrollTop : $('#conversation').get(0).scrollHeight
+		}, 1500);
 	}else{
-		$('#conversation').append('<div class="row message-body"><div class="col-sm-12 message-main-receiver"><div class="receiver"><div class="message-text">'+
-				privateMessage.content + '</div><span class="message-time pull-right">' + dateTime + '</span></div></div></div>');
+		//show new message notification
 	}
-	$('#conversation').animate({
-	    scrollTop: $('#conversation').get(0).scrollHeight
-	}, 1500);
 }
 
-function handleContacts(receivedContacts){
+function handleContacts(receivedContacts) {
 	console.log("From:/topic/contacts-->" + receivedContacts.body);
 	contacts = JSON.parse(receivedContacts.body);
 	insertContacts();
 }
 
-function insertContacts(){
-	for(var i = 0; i < contacts.length; i++){
-		$('#allContactDiv').append('<div'+' id='+i+' onclick="fetchMessages(this.id)" class="row sideBar-body"><div class="col-sm-3 col-xs-3 sideBar-avatar"><div class="avatar-icon"><img src="http://shurl.esy.es/y"></img></div></div><div class="col-sm-9 col-xs-9 sideBar-main"><div class="row"><div class="col-sm-8 col-xs-8 sideBar-name"><span class="name-meta">'
-				+ contacts[i].username + '</span></div><div class="col-sm-4 col-xs-4 pull-right sideBar-time"><span class="time-meta pull-right">18:18 </span></div></div></div></div>');
+function insertContacts() {
+	$('#allContactDiv').html("");
+	for (var i = 0; i < contacts.length; i++) {
+		$('#allContactDiv')
+				.append(
+						'<div'
+								+ ' id='
+								+ i
+								+ ' onclick="fetchMessages(this.id)" class="row sideBar-body"><div class="col-sm-3 col-xs-3 sideBar-avatar"><div class="avatar-icon"><img src="http://shurl.esy.es/y"></img></div></div><div class="col-sm-9 col-xs-9 sideBar-main"><div class="row"><div class="col-sm-8 col-xs-8 sideBar-name"><span class="name-meta">'
+								+ contacts[i].username
+								+ '</span></div><div class="col-sm-4 col-xs-4 pull-right sideBar-time"><span class="time-meta pull-right">18:18 </span></div></div></div></div>');
 	}
 }
 
-function fetchMessages(index){
+function fetchMessages(index) {
 	console.log("Fetching..." + index);
-	console.log("Contact:" + contacts[index]);
+	console.log("Contact:" + JSON.stringify(contacts[index]));
 	contact = contacts[index];
 	currentChatUserId = contacts[index].id;
 	currentChatUser = contacts[index].username;
 	$("#currentChatUser").html(currentChatUser);
+	console.log("Select chatUser details:" + JSON.stringify(contact));
 	getAllMessages(contact);
 }
 
-function getAllMessages(contact){
+function getAllMessages(contact) {
 	stompClient.send("/app/getAllMessages", {}, JSON.stringify({
 		'sendername' : username,
 		'receivername' : contact.username
@@ -126,36 +156,112 @@ function sendMessage() {
 	message = $("#comment").val();
 	console.log("Sending message");
 	stompClient.send("/app/message", {}, JSON.stringify({
-		"senderId": id,
+		"senderId" : id,
 		"receiverId" : currentChatUserId,
-		"toUserOrGroup": true,
+		"toUserOrGroup" : true,
 		"content" : message
 	}));
 	$("#comment").val("");
-	/*$('#conversation').append('<div class="row message-body"><div class="col-sm-12 message-main-sender"><div class="sender"><div class="message-text">'+
-			message + '</div><span class="message-time pull-right"> Sun </span></div></div></div>');
-	$("#comment").val("");
-	$('#conversation').animate({
-	    scrollTop: $('#conversation').get(0).scrollHeight
-	}, 1500);*/
 }
 
-function searchUsers(){
+function searchUsers() {
 	var keyword = $("#keyword").val();
 	console.log("Searching for:" + keyword);
 	stompClient.send("/app/search", {}, JSON.stringify({
-		"keyword": keyword,
-		"username": username
+		"keyword" : keyword,
+		"username" : username
 	}));
 }
 
-function handleSearch(result){
+function handleSearch(result) {
 	$("#searchResults").html("");
 	console.log(result.body);
-	result = JSON.parse(result.body);
-	for(var i = 0; i < result.length; i++){
-		$("#searchResults").append('<div class="row sideBar-body"><div class="col-sm-3 col-xs-3 sideBar-avatar"><div class="avatar-icon"><img src="http://shurl.esy.es/y"></img></div></div><div class="col-sm-9 col-xs-9 sideBar-main"><div class="row"><div class="col-sm-8 col-xs-8 sideBar-name"><span class="name-meta">'+
-				result[i].username + '</span></div><div class="col-sm-4 col-xs-4 pull-right sideBar-time"><span class="time-meta pull-right">18:18 </span></div></div></div></div>');
+	searchResults = JSON.parse(result.body);
+	for (var i = 0; i < searchResults.length; i++) {
+		$("#searchResults")
+				.append(
+						'<div id="'
+								+ i
+								+ '" onclick="addContact(this.id)" class="row sideBar-body"><div class="col-sm-3 col-xs-3 sideBar-avatar"><div class="avatar-icon"><img src="http://shurl.esy.es/y"></img></div></div><div class="col-sm-9 col-xs-9 sideBar-main"><div class="row"><div class="col-sm-8 col-xs-8 sideBar-name"><span class="name-meta">'
+								+ searchResults[i].username
+								+ '</span></div><div class="col-sm-4 col-xs-4 pull-right sideBar-time"><span class="time-meta pull-right">18:18 </span></div></div></div></div>');
+	}
+}
+
+function addContact(index) {
+	// hide search panel
+	$(".side-two").css({
+		"left" : "-100%"
+	});
+	var selectedContact = searchResults[index];
+	contacts.push(selectedContact);
+	console.log(selectedContact);
+	$('#allContactDiv')
+			.append(
+					'<div onclick="fetchMessages(this.id)" class="row sideBar-body"><div class="col-sm-3 col-xs-3 sideBar-avatar"><div class="avatar-icon"><img src="http://shurl.esy.es/y"></img></div></div><div class="col-sm-9 col-xs-9 sideBar-main"><div class="row"><div class="col-sm-8 col-xs-8 sideBar-name"><span class="name-meta">'
+							+ selectedContact.username
+							+ '</span></div><div class="col-sm-4 col-xs-4 pull-right sideBar-time"><span class="time-meta pull-right">18:18 </span></div></div></div></div>');
+	currentChatUserId = selectedContact.id;
+	currentChatUser = selectedContact.username;
+	$("#currentChatUser").html(currentChatUser);
+	stompClient.send("/app/addContact", {}, JSON.stringify({
+		"userId" : id,
+		"contactId" : selectedContact.id
+	}));
+	getAllMessages(selectedContact);
+}
+
+function filterIt(objects, searchKey) {
+	var results = [];
+
+	var toSearch = searchKey;
+
+	for (var i = 0; i < objects.length; i++) {
+		if (objects[i].username.indexOf(toSearch) != -1) {
+			results.push(objects[i]);
+		}
+	}
+	return results;
+}
+
+function fetchMessagesForSearchedUser(index) {
+	for (var i = 0; i < contacts.length; i++) {
+		if (contactSearchResults[index].username == contacts[i].username) {
+			fetchMessages(i);
+			break;
+		}
+	}
+}
+
+function searchContacts() {
+	$('#allContactDiv').html("");
+	var keyword = $("#searchContacts").val();
+	console.log("Searching contacts for:" + keyword);
+	if (keyword.length > 0) {
+		contactSearchResults = filterIt(contacts, keyword);
+		console.log(contactSearchResults);
+		for (var i = 0; i < contactSearchResults.length; i++) {
+			$('#allContactDiv')
+					.append(
+							'<div'
+									+ ' id='
+									+ i
+									+ ' onclick="fetchMessagesForSearchedUser(this.id)" class="row sideBar-body"><div class="col-sm-3 col-xs-3 sideBar-avatar"><div class="avatar-icon"><img src="http://shurl.esy.es/y"></img></div></div><div class="col-sm-9 col-xs-9 sideBar-main"><div class="row"><div class="col-sm-8 col-xs-8 sideBar-name"><span class="name-meta">'
+									+ contactSearchResults[i].username
+									+ '</span></div><div class="col-sm-4 col-xs-4 pull-right sideBar-time"><span class="time-meta pull-right">18:18 </span></div></div></div></div>');
+		}
+	} else {
+		// show all contacts
+		for (var i = 0; i < contacts.length; i++) {
+			$('#allContactDiv')
+					.append(
+							'<div'
+									+ ' id='
+									+ i
+									+ ' onclick="fetchMessages(this.id)" class="row sideBar-body"><div class="col-sm-3 col-xs-3 sideBar-avatar"><div class="avatar-icon"><img src="http://shurl.esy.es/y"></img></div></div><div class="col-sm-9 col-xs-9 sideBar-main"><div class="row"><div class="col-sm-8 col-xs-8 sideBar-name"><span class="name-meta">'
+									+ contacts[i].username
+									+ '</span></div><div class="col-sm-4 col-xs-4 pull-right sideBar-time"><span class="time-meta pull-right">18:18 </span></div></div></div></div>');
+		}
 	}
 }
 
@@ -167,19 +273,19 @@ function getURLParameter(name) {
 }
 
 function addZero(i) {
-    if (i < 10) {
-        i = "0" + i;
-    }
-    return i;
+	if (i < 10) {
+		i = "0" + i;
+	}
+	return i;
 }
 
 function getFormattedDateTime(dateTime) {
-    var d = dateTime;
-    var date = addZero(d.getDate());
-    var month = addZero(d.getMonth()+1);
-    var year = addZero(d.getFullYear());
-    var h = addZero(d.getHours());
-    var m = addZero(d.getMinutes());
-    var s = addZero(d.getSeconds());
-    return h + ":" + m + ":" + s;// + " " + date + "/" + month + "/" + year;
+	var d = dateTime;
+	var date = addZero(d.getDate());
+	var month = addZero(d.getMonth() + 1);
+	var year = addZero(d.getFullYear());
+	var h = addZero(d.getHours());
+	var m = addZero(d.getMinutes());
+	var s = addZero(d.getSeconds());
+	return h + ":" + m + ":" + s;// + " " + date + "/" + month + "/" + year;
 }
